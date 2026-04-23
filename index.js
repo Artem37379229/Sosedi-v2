84024181b0d07c84de4321132f3ed86187ee014c
Artem <artembonda487@gmail.com>
2026-04-23 16:17:19 +0300

    init: init project

---
+++ index.js
@@ -0,0 +1,16 @@
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.header__nav');
+const menu = document.querySelector('.header__menu');
+const heroBtn = document.querySelector('.hero__button');
+const headerAccount = document.querySelector('.header__account');
+
+const body = document.querySelector('body');
+
+burger.addEventListener('click', () => {
+    body.classList.toggle('active');
+    burger.classList.toggle('active');
+    nav.classList.toggle('active');
+    menu.classList.toggle('active');
+    heroBtn.classList.toggle('active');
+    headerAccount.classList.toggle('active');
+});
